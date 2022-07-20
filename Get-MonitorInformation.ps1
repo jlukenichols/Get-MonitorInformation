04265f41186a8756febb28a2c7973994f1229c65
jlukenichols <J.Luke.Nichols@gmail.com>
2022-07-20 14:37:37 -0400

    Update Get-MonitorInformation.ps1
    Added additional manufacturer codes

--- Get-MonitorInformation.ps1
+++ Get-MonitorInformation.ps1
@@ -9,14 +9,34 @@ foreach ($Computer in $ComputerName.Split(",").Trim()) {
             $ManufacturerName = "Dell"
         } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACI") {
             $ManufacturerName = "ASUS"
-        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SEC") {
-            $ManufacturerName = "Epson"
         } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACR") {
             $ManufacturerName = "Acer"
-        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "UGD") {
-            $ManufacturerName = "XP-PEN"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "AUS") {
+            $ManufacturerName = "ASUS"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "BNQ") {
+            $ManufacturerName = "BenQ"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "CEI") {
+            $ManufacturerName = "Crestron"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "CTX") {
+            $ManufacturerName = "Citrix"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "GSM") {
+            $ManufacturerName = "LG"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "HPN") {
+            $ManufacturerName = "HP"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "MEI") {
+            $ManufacturerName = "Panasonic"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "NVD") {
+            $ManufacturerName = "Nvidia"
         } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SAM") {
             $ManufacturerName = "Samsung"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SEC") {
+            $ManufacturerName = "Epson"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SPT") {
+            $ManufacturerName = "Sceptre"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "UGD") {
+            $ManufacturerName = "XP-PEN"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "VSC") {
+            $ManufacturerName = "Viewsonic"
         } else {
             $ManufacturerName = ($_.ManufacturerName | ForEach-Object { [char]$_ }) -join ""
         }
