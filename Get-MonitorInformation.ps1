28c73274f551417c6e0d3d3d86768f0cd890062c
jlukenichols <J.Luke.Nichols@gmail.com>
2022-07-20 13:56:23 -0400

    Update Get-MonitorInformation.ps1

--- Get-MonitorInformation.ps1
+++ Get-MonitorInformation.ps1
@@ -4,29 +4,29 @@
 Start-Service "WinRM" -ErrorAction SilentlyContinue | Out-Null    
 
 foreach ($Computer in $ComputerName.Split(",").Trim()) {
-        Get-CimInstance -ComputerName $Computer -ClassName WmiMonitorID -Namespace root\wmi | Foreach-Object {
-            if ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "DEL") {
-                $ManufacturerName = "Dell"
-            } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACI") {
-                $ManufacturerName = "ASUS"
-            } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SEC") {
-                $ManufacturerName = "Epson"
-            } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACR") {
-                $ManufacturerName = "Acer"
-            } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "UGD") {
-                $ManufacturerName = "XP-PEN"
-            } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SAM") {
-                $ManufacturerName = "Samsung"
-            } else {
-                $ManufacturerName = ($_.ManufacturerName | ForEach-Object { [char]$_ }) -join ""
-            }
-            [PSCustomObject]@{
-                'Active'              = $_.Active
-                'Manufacturer'        = $ManufacturerName
-                'Model'               = ($_.UserFriendlyName | ForEach-Object { [char]$_ }) -join ""
-                'Serial Number'       = ($_.SerialNumberID | ForEach-Object { [char]$_ }) -join ""
-                'Year Of Manufacture' = $_.YearOfManufacture
-                'Week Of Manufacture' = $_.WeekOfManufacture
-            }
-        }        
+    Get-CimInstance -ComputerName $Computer -ClassName WmiMonitorID -Namespace root\wmi | Foreach-Object {
+        if ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "DEL") {
+            $ManufacturerName = "Dell"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACI") {
+            $ManufacturerName = "ASUS"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SEC") {
+            $ManufacturerName = "Epson"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "ACR") {
+            $ManufacturerName = "Acer"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "UGD") {
+            $ManufacturerName = "XP-PEN"
+        } elseif ((($_.ManufacturerName | ForEach-Object { [char]$_ }) -join "") -eq "SAM") {
+            $ManufacturerName = "Samsung"
+        } else {
+            $ManufacturerName = ($_.ManufacturerName | ForEach-Object { [char]$_ }) -join ""
+        }
+        [PSCustomObject]@{
+            'Active'              = $_.Active
+            'Manufacturer'        = $ManufacturerName
+            'Model'               = ($_.UserFriendlyName | ForEach-Object { [char]$_ }) -join ""
+            'Serial Number'       = ($_.SerialNumberID | ForEach-Object { [char]$_ }) -join ""
+            'Year Of Manufacture' = $_.YearOfManufacture
+            'Week Of Manufacture' = $_.WeekOfManufacture
+        }
+    }        
 }
